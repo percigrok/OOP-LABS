65b2a284d7693bc077da1874dd6c3ed651c50fba
IGROK <vadimorus@gmail.com>
2026-09-16 12:01:56 +0500

    Добавил  printArray function - функция вывода массива

--- LAB1/task1.cpp
+++ LAB1/task1.cpp
@@ -10,3 +10,16 @@ void fillArray(int (&arr)[10])
         std::cin >> arr[i];
     }
 }
+
+/// Выводит элементы массива на экран
+/// @param arr Ссылка на массив из 10 целых чисел
+/// @details Использует range-based for цикл с ключевым словом auto для вывода всех элементов
+void printArray(int (&arr)[10])
+{
+    for (auto x : arr)
+    {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+}
+
