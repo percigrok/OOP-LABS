54977561b5bc73eebf911c2c22dd239ee9a401ac
IGROK <vadimorus@gmail.com>
2026-09-20 15:05:40 +0500

    Add fillMatrix function - функция заполнения матрицы

--- LAB1/task4.cpp
+++ LAB1/task4.cpp
@@ -19,3 +19,21 @@ int** allocateMatrix(int rows, int cols)
     }
     return matrix;
 }
+
+/**
+ * @brief Заполняет матрицу значениями со стандартного ввода
+ * @param matrix Указатель на матрицу (указатель на массив указателей)
+ * @param rows Количество строк
+ * @param cols Количество столбцов
+ * @details Читает rows*cols значений в порядке по строкам
+ */
+void fillMatrix(int** matrix, int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            std::cin >> matrix[i][j];
+        }
+    }
+}
